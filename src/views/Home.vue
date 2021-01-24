3c3eea4422265b6a4a4b24b0d10179cff6b08586
Noah Hessel <hesselapplications@gmail.com>
2021-01-24 07:30:07 -0700

    Clean up templates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <RentalCalculator/>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import RentalCalculator from "@/components/RentalCalculator.vue";
 
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
-  }
-}
+    RentalCalculator,
+  },
+};
 </script>
